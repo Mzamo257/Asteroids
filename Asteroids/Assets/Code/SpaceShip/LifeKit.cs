5163953cfc681064906ee37851cccd42cd6231d6
ukibs <martincativiela@gmail.com>
2018-04-12 14:10:01 +0200

    Añadidos medkits para survival (en feo)

---
+++ Asteroids/Assets/Code/SpaceShip/LifeKit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeKit : MonoBehaviour {
+
+    //
+    public int amountToHeal = 50;
+
+    //
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.name == "SpaceShip")
+        {
+            SurvivalLevelManager levelMgr = FindObjectOfType<SurvivalLevelManager>();
+            levelMgr.SpaceCurrentLife += amountToHeal;
+            gameObject.SetActive(false);
+        }
+    }
+}
