e86c6ad27abbe4d1b03d90f484500047ffc845ce
LAPTOP-6855M5OO\Cristina <criix_rubi@hotmail.com>
2018-04-19 16:07:32 +0200

    He modificado el movimiento de la nave he arreglado el primer empujon y ahora esquiva aleatoriamente hacia arriba o hacia abajo.

--- Asteroids/Assets/Code/SpaceShip/SurvivalMovementSpaceship.cs
+++ Asteroids/Assets/Code/SpaceShip/SurvivalMovementSpaceship.cs
@@ -30,21 +30,8 @@ public class SurvivalMovementSpaceship : BaseSpaceship {
 
 	// Update is called once per frame
 	protected override void Update () {
-		if (Physics.SphereCast (transform.position, maxDetectionDistance, rb.velocity, out hitInfo)) {
-			if(hitInfo.transform.tag.Equals("Asteroid"))
-			{
-				Debug.Log ("Sphere Detection");
-				if (Physics.Raycast (transform.position, rb.velocity, out hitInfo, maxDetectionDistance)) {
-
-					if(hitInfo.transform.tag.Equals("Asteroid"))
-					{
-						rb.AddForce (transform.up, ForceMode.Impulse);
-						Debug.Log ("Raycast Detection");
-					}
-				}
-			}
-		}
-
+		
+		dodge ();
 		velocity = rb.velocity;
 		currentUpdateTime += Time.deltaTime;
 		angleResultant = Vector3.Angle (velocity, posNextWayPointRelative);
@@ -77,16 +64,34 @@ public class SurvivalMovementSpaceship : BaseSpaceship {
 		}
 	}
 
+	private void dodge()
+	{
+		if (Physics.SphereCast (transform.position, maxDetectionDistance, rb.velocity, out hitInfo)) {
+			if(hitInfo.transform.tag.Equals("Asteroid"))
+			{
+				Debug.Log ("Sphere Detection");
+				if (Physics.Raycast (transform.position, rb.velocity, out hitInfo, maxDetectionDistance)) {
+
+					if(hitInfo.transform.tag.Equals("Asteroid"))
+					{
+						int random = UnityEngine.Random.Range (0,1);
+						if (random == 0) {
+							rb.AddForce (transform.up, ForceMode.Impulse);
+						} else if(random == 1){
+							rb.AddForce (-transform.up, ForceMode.Impulse);
+						}
+							
+						Debug.Log ("Raycast Detection");
+					}
+				}
+			}
+		}
+	}
+
 	private void Redirect()
 	{
-		//Hay que arreglar este que es el primer empujon de la nave
-		if (!firstMovement) {
-			rb.AddForce (posNextWayPointRelative.normalized * force, ForceMode.Impulse);
-			firstMovement = true;
-			previousRotation = nextRotation;
-			nextRotation = Quaternion.LookRotation (posNextWayPointRelative);
-		} 
-		else if(secondMovement && currentUpdateTime > updateTime)
+		
+		if(currentUpdateTime > updateTime)
 		{
 			adjustedDirection = adjustDirection(velocity, posNextWayPointRelative).normalized;
 			rb.AddForce (adjustedDirection * force, ForceMode.Impulse);
@@ -108,7 +113,11 @@ public class SurvivalMovementSpaceship : BaseSpaceship {
 	{
 		if (rb.velocity.y > 0.1f) {
 			rb.AddForce (Vector3.down, ForceMode.Force);
-			Debug.Log ("VerticalSpeedControl");
+			Debug.Log ("VerticalSpeedControl_hacia abajo");
+		} else if (rb.velocity.y < -0.1) {
+			rb.AddForce (Vector3.up, ForceMode.Force);
+			Debug.Log ("VerticalSpeedControl_ hacia arriba");
+
 		}
 	}
 	#endregion
